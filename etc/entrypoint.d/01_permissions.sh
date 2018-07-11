7f2e93a24f234fb923e0e4e5f2fc70971e9c07c9
Free Man <admin@wolnosciowiec.org>
2018-07-11 07:14:29 +0200

    Optimized permissions setting on entrypoint

--- etc/entrypoint.d/01_permissions.sh
+++ etc/entrypoint.d/01_permissions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo " >> Setting up Symfony-specific permissions"
-mkdir -p /var/www/vendor /var/www/var/cache
-chown www-data:www-data /var/www/vendor /var/www/var /var/www/public /var/www/web -R
+mkdir -p /var/www/html/vendor /var/www/html/var/cache
+chown www-data:www-data /var/www/html/vendor /var/www/html/var /var/www/html/public /var/www/html/web -R
