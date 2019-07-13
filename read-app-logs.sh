2355e628b07f0c352cd8fcd533d340bd9b91a847
Free Man <admin@wolnosciowiec.org>
2019-07-13 08:17:41 +0200

    Supervisor will now redirect stdout and stderr by default, and will display application logs

--- read-app-logs.sh
+++ read-app-logs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-while [[ ! -f /var/www/html/var/log/prod.log ]]; do
+LOG_FILE_NAME=${APP_ENV:-prod}
+
+while [[ ! -f "/var/www/html/var/log/${LOG_FILE_NAME}.log" ]]; do
     sleep 1
 done
 
-exec tail -f /var/www/html/var/log/prod.log
+exec tail -f "/var/www/html/var/log/${LOG_FILE_NAME}.log"
