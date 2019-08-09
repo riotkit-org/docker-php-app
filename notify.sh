5a57f6b99a588ee593502990eaeda9d7a04afd84
Free Man <admin@wolnosciowiec.org>
2019-08-09 19:57:57 +0200

    Improved notifications

--- notify.sh
+++ notify.sh
@@ -3,6 +3,6 @@
 SLACK_URL=$1
 MESSAGE=$2
 
-[[ "$SLACK_URL" ]] && echo "{\"text\": \"${MESSAGE}\"}" | curl -H 'Content-type: application/json' -d @- -X POST -s "${SLACK_URL}";
+[[ "$SLACK_URL" ]] && echo "{\"text\": \"[Travis-CI] ${MESSAGE}\"}" | curl -H 'Content-type: application/json' -d @- -X POST -s "${SLACK_URL}";
 
 exit 0
